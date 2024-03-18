579ba3ce92aca18fb4ad8d8e5ed55ce68c9de042
aravinddibbada <daravindkkl@gmail.com>
2024-03-18 12:31:05 +0530

    updated

--- DBUTILS.java
+++ DBUTILS.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 this is the new file
 im gngn to update this file
@@ -5,3 +6,8 @@ im gngn to update this file
  Hello this file contains the DB connection logic...
 HEllo good morning .updating file in dev branch...
 >>>>>>> development
+=======
+Hello ... iam updating this file... 
+Hello this file contains the DB connection logic...
+HEllo
+>>>>>>> Stashed changes
